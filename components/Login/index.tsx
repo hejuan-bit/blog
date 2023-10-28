82b3efa5abafc2d08a2994b87734d3a4c17f6c63
hejuan-bit <1493826046@qq.com>
2023-10-29 00:18:10 +0800

    新政短信验证功能

--- components/Login/index.tsx
+++ components/Login/index.tsx
@@ -2,6 +2,8 @@ import React, { ChangeEvent } from "react";
 import style from './index.module.scss';
 import {useState} from "react";
 import CountDown from '../CountDown';
+import {message} from 'antd';
+import request from '../../service/fetch'
 
 interface Iprops {
     isShow: boolean;
@@ -25,7 +27,13 @@ const Login = (props: Iprops) => {
     }
 
     const getVerifyCode = () => {
-        setIsShowVerifyCode(true)
+        if(!form?.phone){
+            message.warning('请输入手机号');
+            return;
+        }
+        request.post('/api/user/senVerifyCode');
+
+        // setIsShowVerifyCode(true)
     }
 
     const handleLogin = () => {
@@ -56,14 +64,14 @@ const Login = (props: Iprops) => {
                     <div>手机登录页</div>
                     <div className={style.close} onClick={handleClose}>X</div>
                 </div>
-                <input type="text" name="phone" placeholder="请输入手机号" value={form.phone}/>
+                <input type="text" name="phone" placeholder="请输入手机号" value={form.phone} onChange={handleFormChange}/>
                 <div className={style.verifycodeArea}>
                     <input type="text" name="verify" placeholder="请输入验证码" value={form.verify} onChange={handleFormChange}/>
                     <span className={style.verifyCode} onClick={getVerifyCode}>
                         {isShowVerifyCode ? <CountDown time={10} onEnd={handleCountDownEnd}/> : "获取验证码"}
                     </span>
                 </div>
-                <div className={style.loginBtn} onClick={handleLogin} onChange={handleFormChange}>登录</div>
+                <div className={style.loginBtn} onClick={handleLogin}>登录</div>
                 <div className={style.otherLogin} onClick={handleOtherLogin}>使用Githab登录</div>
                 <div className={style.loginPrivacy}>
                     注册登录即表示同意
